78a2c70e6d07c8df954f736cf173191c96d338e2
Hojirakhmatullo Kasymov <kasymov_roma@list.ru>
2025-05-31 22:30:55 +0300

    Add GitHub Pages deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/speedrunKURSACH/',
   server: {
     host: true,
     port: 8080,
